5ec2f4e6b2d46cacde87966115d9bc72abda3993
eyejava <eyejava@163.com>
2019-11-07 21:10:21 +0800

    增加爬虫

---
+++ src/main/java/com/jade/showcase/model/User.java
@@ -0,0 +1,15 @@
+package com.jade.showcase.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private  String name;
+
+    private  Integer age;
+
+    private Integer salary;
+
+
+}
---
+++ src/test/java/com/jade/showcase/demo/change/DemoTest.java
@@ -0,0 +1,8 @@
+package com.jade.showcase.demo.change;
+
+import static org.junit.Assert.*;
+
+public class DemoTest {
+
+
+}
